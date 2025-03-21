a43ae3057eee4586e895ad8d13523cba682a6094
Maikel Marín <maikel.marin10@gmail.com>
2025-03-20 22:11:03 -0600

    Maikel

--- ARMONIA DIGITAL/armonia_digital/lib/main.dart
+++ ARMONIA DIGITAL/armonia_digital/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Technology Company',
+      title: 'Technology Company123444',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
